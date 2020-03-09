c4819b26d82fd30171673ca7908082f2c5701211
melanie-t <melanie.taing@mail.concordia.ca>
2020-03-09 17:21:49 -0400

    Generate topics using spotlight-set.txt

--- src/rdfPopulator/TopicsPopulator.py
+++ src/rdfPopulator/TopicsPopulator.py
@@ -1,34 +1,34 @@
-from rdflib import URIRef, Literal
+from rdflib import URIRef, Literal, Graph
 from rdflib.namespace import RDF, RDFS
-from src.spotlightAnnotations import SpotlightAssociations
 
 
-# Find spotlight associations for specific course URI
-def init_topics(graph, course_uri_set):
-    topic_namespace_uri = "http://www.example.org/topic/"
+# Generate topics using DBPedia Spotlight, we will use the generated text files to add topic triples
+def init_topics(graph):
     sioc_namespace = "http://rdfs.org/sioc/ns#"
+    topic_namespace_uri = "http://www.example.org/topic/"
 
-    for course in course_uri_set:
-        print(course)
-        try:
-            termLink = SpotlightAssociations.SpotlightAssociations.get_keywords_from_text(
-                course[1] + " " + course[2])
+    # Add Topic triples
+    all_topics = open("../spotlightAnnotations/spotlight-set.txt").readlines()
+    for topic in all_topics:
+        topic.replace("\\n", "")
+        topic_arg = topic.split("\" ")
+        label = topic_arg[0].replace("\"", "")
 
-            if len(termLink) != 0:
-                for entity in termLink:
-                    label = entity[0]
-                    uri = entity[1]
-                    graph.add((URIRef(course[0]), URIRef(sioc_namespace + "topic"), URIRef(uri)))
-                    graph.add((URIRef(uri), RDF.type, URIRef(topic_namespace_uri)))
-                    graph.add((URIRef(uri), RDFS.label, Literal(label)))
+        uri = topic_arg[1]
+        graph.add((URIRef(uri), RDF.type, URIRef(topic_namespace_uri)))
+        graph.add((URIRef(uri), RDFS.label, Literal(label)))
 
-                    output_file = open("spotlight.txt", 'r')
-                    association = (str(course[0]) + " " + str(label) + " " + str(uri))
-                    output_file.write(association)
-                    print("Saved to file" + association)
-                    output_file.close()
+    # Adding Topics for Courses
+    all_courses = open("../spotlightAnnotations/spotlight-courses.txt").readlines()
+    for course in all_courses:
+        course.replace("\\n", "")
+        # course_arg = course.split(" ")
+        # course_uri = course_arg[0]
+        # topic_uri = course_arg[2]
+        # graph.add((URIRef(course_uri), URIRef(sioc_namespace+'topic'), topic_uri))
 
-        except Exception as e:
-            print(e)
 
-    return graph
+# graph = Graph()
+# init_topics(graph)
+# for row in graph:
+#     print(row)
