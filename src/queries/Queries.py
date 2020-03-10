fa2f2950bc42ef54560f65a4673bc3699daf88bf
melanie-t <melanie.taing@mail.concordia.ca>
2020-03-09 21:11:11 -0400

    Naming convention for queries

--- src/queries/Queries.py
+++ src/queries/Queries.py
@@ -1,61 +1,68 @@
 import os
 import errno
-
+from rdflib import Graph
 
 class Q1:
     # 1. Total number of triples in the KB
     @staticmethod
     def query(prefix, graph):
-        query, output = open_query_files("q1")
+        query_num = "q1"
+        query, output = open_query_files(query_num)
         query_string = query.read()
         query_string = prefix + query_string
         print("Query 1\n" + query_string)
 
+        construct_results = Graph()
         results = graph.query(query_string)
-        save_results(results, output)
+        save_results(results, output, query_num)
 
 
 class Q2:
     # 2. Total number of students, courses, and topics
     @staticmethod
     def query(prefix, graph):
-        query, output = open_query_files("q2")
+        query_num = "q2"
+        query, output = open_query_files(query_num)
         query_string = query.read()
         query_string = prefix + query_string
         print("Query 2\n" + query_string)
 
+        construct_results = Graph()
         results = graph.query(query_string)
-        save_results(results, output)
+        save_results(results, output, query_num)
 
 
 class Q3:
     # 3. For a course c, list all covered topics using their (English) labels and their link to DBpedia
     @staticmethod
     def query(course_uri, prefix, graph):
-        # Query 1. Total number of triples in the KB
-        query, output = open_query_files("q3")
+        query_num = "q3"
+        query, output = open_query_files(query_num)
         query_string = query.read()
         query_string = query_string.replace("{course_uri}", str(course_uri))
         query_string = prefix + query_string
         print("Query 3\n" + query_string)
 
+        construct_results = Graph()
         results = graph.query(query_string)
-        save_results(results, output)
+        save_results(results, output, query_num)
 
 
 class Q4:
     # 4. For a given student, list all courses this student completed, together with the grade
     @staticmethod
     def query(student_id, prefix, graph):
-        query, output = open_query_files("q4")
+        query_num = "q4"
+        query, output = open_query_files(query_num)
         query_string = query.read()
         query_string = prefix + query_string
         # Replace values
         query_string = query_string.replace("{student_id}", str(student_id))
         print("Query 4\n" + query_string)
 
+        construct_results = Graph()
         results = graph.query(query_string)
-        save_results(results, output)
+        save_results(results, output, query_num)
 
 
 class Q5:
@@ -63,15 +70,16 @@ class Q5:
     # and did not fail, a course that covered the topic
     @staticmethod
     def query(student_id, prefix, graph):
-        query, output = open_query_files("q5")
+        query_num = "q5"
+        query, output = open_query_files(query_num)
         query_string = query.read()
         query_string = prefix + query_string
         # Replace values
 
         print("Query 5\n" + query_string)
-
+        results = Graph()
         results = graph.query(query_string)
-        save_results(results, output)
+        save_results(results, output, query_num)
 
 
 class Q6:
@@ -79,31 +87,33 @@ class Q6:
     # (based on the completed courses for this student that are better than an \F" grade)
     @staticmethod
     def query(student_id, prefix, graph):
-        query, output = open_query_files("q6")
+        query_num = "q6"
+        query, output = open_query_files(query_num)
         query_string = query.read()
         query_string = prefix + query_string
         # Replace values
 
         print("Query 6\n" + query_string)
-
+        results = Graph()
         results = graph.query(query_string)
-        save_results(results, output)
+        save_results(results, output, query_num)
 
 
-def open_query_files(query_number):
-    query_file_path = "./sparql_queries/" + f"py_{query_number}.txt"
-    output_file_path = "./output/" + f"{query_number}_response.txt"
+def open_query_files(query_num):
+    query_file_path = "./sparql_queries/" + f"{query_num}.txt"
+    output_file_path = "./output/" + f"{query_num}_response.txt"
 
     make_sure_path_exists(query_file_path)
     make_sure_path_exists(output_file_path)
 
     query = open(query_file_path, "r")
-    output = open(output_file_path, "w")
+    output = open(output_file_path, "r")
 
     return query, output
 
 
-def save_results(results, output_file):
+def save_results(results, output_file, query_num):
+    output_ttl = str(query_num) + ".ttl"
     for row in results:
         result = ''
         for entry in row:
