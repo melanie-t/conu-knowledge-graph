42f4c7e7379832be2d0fbd1b690e73696acc79e8
Durandan5 <Durandan5@users.noreply.github.com>
2020-04-15 17:10:31 -0400

    output of queries

--- src/chatbot/Main.py
+++ src/chatbot/Main.py
@@ -1,7 +1,10 @@
 from src.chatbot.inputParser import parseInput
-
+from rdflib import Graph
 
 def main():
+    g = Graph()
+    output_file = "../rdfPopulator/output.ttl"
+    g.parse(output_file, format="turtle")
     print("Hi there, I am ConU ChatBot :)")
     while True:
 
@@ -11,11 +14,22 @@ def main():
         q4 = "Who is familiar with Education?"
 
         questionInput = input("What would you like to know? ")
+        #questionInput = q1
         query = parseInput(questionInput)
         print(query)
-
+        results = Graph()
+        results = g.query(query)
+        #print(results)
+        i = 1
+        for row in results:
+            result = ''
+            for entry in row:
+                result = result + str(entry) + ' '
+            result = result + '\n'
+            print(i , ":", result)
+            i = i + 1
 
 if __name__ == "__main__":
-    main()
+    main();
 
 
